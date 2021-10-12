8b0aa7dc20a6c31e55492601f77c6df07cbcafbe
Sterling Watts <sterling.watts@mindera.com>
2021-10-11 19:38:41 -0700

    Create user and auth endpoints

--- test/setup.js
+++ test/setup.js
@@ -1,13 +1,12 @@
-process.env.TZ = "UTC";
-process.env.NODE_ENV = "test";
-process.env.JWT_SECRET = "test-jwt-secret";
-process.env.JWT_EXPIRY = "3m";
-process.env.TEST_DATABSE_URL =
-  process.env.TEST_DATABASE_URL || "postgresql://example-test";
-
 require("dotenv").config();
 const { expect } = require("chai");
 const supertest = require("supertest");
+
+process.env.TZ = "UTC";
+process.env.NODE_ENV = "test";
+process.env.JWT_SECRET = process.env.JWT_SECRET;
+process.env.JWT_EXPIRY = "3m";
+process.env.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || "postgresql://sterling.watts@localhost/poker-test";
 
 global.expect = expect;
 global.supertest = supertest;
